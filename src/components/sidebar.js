ea7453c7e1036a6eea7098604450583a16d6f1c0
natagoh <natagoh@stanford.edu>
2019-12-01 15:20:41 -0800

    icon spacing

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -9,9 +9,9 @@ class Sidebar extends React.Component {
     return (
     	<div class="sidebar">
 		  	<ul>
-		  		<li><a href="#"><FontAwesomeIcon icon={faHome} className="mr-4"/>{' '}Home</a></li>
-		  		<li><a href="#"><FontAwesomeIcon icon={faCog} className="mr-4"/>Patients</a></li>
-		  		<li><a href="#"><FontAwesomeIcon icon={faHome} className="mr-4"/>Stats</a></li>
+		  		<li><a href="#"><FontAwesomeIcon icon={faHome} className="mr"/>Home</a></li>
+		  		<li><a href="#"><FontAwesomeIcon icon={faCog} className="mr"/>Patients</a></li>
+		  		<li><a href="#"><FontAwesomeIcon icon={faHome} className="mr"/>Stats</a></li>
 		   	</ul>
 	   	</div>
 		);
