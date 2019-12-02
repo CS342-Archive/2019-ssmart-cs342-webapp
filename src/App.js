900350bd3a346b4be57bca16d0c0925884c21d13
natagoh <natagoh@stanford.edu>
2019-12-01 19:39:01 -0800

    connects with firestore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import Firebase from 'firebase'
+import firebase from 'firebase'
 import config from './config'
 
 import Dashboard from './components/dashboard'
@@ -9,23 +9,74 @@ import Dashboard from './components/dashboard'
 class App extends React.Component {
   constructor(props) {
     super(props);
-    Firebase.initializeApp(config);
+    firebase.initializeApp(config);
+    this.db = firebase.firestore();
+
+
+//     var citiesRef = db.collection("cities");
+
+// citiesRef.doc("SF").set({
+//     name: "San Francisco", state: "CA", country: "USA",
+//     capital: false, population: 860000,
+//     regions: ["west_coast", "norcal"] });
+// citiesRef.doc("LA").set({
+//     name: "Los Angeles", state: "CA", country: "USA",
+//     capital: false, population: 3900000,
+//     regions: ["west_coast", "socal"] });
+// citiesRef.doc("DC").set({
+//     name: "Washington, D.C.", state: null, country: "USA",
+//     capital: true, population: 680000,
+//     regions: ["east_coast"] });
+// citiesRef.doc("TOK").set({
+//     name: "Tokyo", state: null, country: "Japan",
+//     capital: true, population: 9000000,
+//     regions: ["kanto", "honshu"] });
+// citiesRef.doc("BJ").set({
+//     name: "Beijing", state: null, country: "China",
+//     capital: true, population: 21500000,
+//     regions: ["jingjinji", "hebei"] });
+
+
+
     this.state = {
       data: []
     }
   }
 
+
+// db.collection("cities").where("state", "==", "CA")
+//     .onSnapshot(function(querySnapshot) {
+//         var cities = [];
+//         querySnapshot.forEach(function(doc) {
+//             cities.push(doc.data().name);
+//         });
+//         console.log("Current cities in CA: ", cities.join(", "));
+//     });
+
+    
+  getUpdate = () => {
+    this.db.collection("test").doc("data")
+      .onSnapshot(function(doc) {
+          var source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+          console.log(source, " data: ", doc.data());
+      });
+  }
+
+
   getData = () => {
-    let ref = Firebase.database().ref('/test');
+    let ref = firebase.database().ref('/test');
     ref.on('value', snapshot => {
       const state = snapshot.val();
+      console.log(state)
       this.setState(state);
     });
     console.log('DATA RETRIEVED');
+
   }
 
   componentDidMount() {
-    this.getData();
+    // this.getData();
+    this.getUpdate();
   }
 
   render() {
